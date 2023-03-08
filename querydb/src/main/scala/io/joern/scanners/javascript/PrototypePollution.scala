6b72e88bb102c24b7148f06131f076e0bab40c96
= <tobias@frobergs.org>
2023-03-08 15:13:29 +0100

    Query updated to focus on objects in what way possible

--- querydb/src/main/scala/io/joern/scanners/javascript/PrototypePollution.scala
+++ querydb/src/main/scala/io/joern/scanners/javascript/PrototypePollution.scala
@@ -8,7 +8,7 @@ import io.joern.macros.QueryMacros._
 import io.joern.dataflowengineoss.language._
 import io.joern.dataflowengineoss.queryengine.EngineContext
 import io.joern.dataflowengineoss.semanticsloader.Semantics
-
+import scala.util.control.Breaks._
 
 object PrototypePollution extends QueryBundle {
 
@@ -26,18 +26,25 @@ object PrototypePollution extends QueryBundle {
         |""".stripMargin,
       score = 8,
       withStrRep({ cpg =>
-        def possiblePollution = cpg.call.where(_.name(Operators.assignment).reachableBy(cpg.method.parameter))
+        def possiblePollution = cpg.call.where(_.name(Operators.assignment).reachableBy(cpg.method.parameter.nameNot("this")))
         var idList : List[Long] = List()
         for (pp <- possiblePollution) {
-          def methodName = pp.location.methodShortName
-          def valReachable = pp.argument(2).reachableBy(cpg.method.name(methodName).parameter)
-          def indexAccessInAssignment = pp.astChildren.order(1).isCall.name(Operators.indexAccess)
-          def indexArgument = indexAccessInAssignment.argument(2)
-          def indexArgumentTainted = indexArgument.where(_.reachableBy(cpg.method.name(methodName).parameter))
-          def identifierOrCall = indexAccessInAssignment.argument(1)
-          def lastArgumentTainted = identifierOrCall.reachableBy(cpg.call(Operators.indexAccess)).where(_.argument(2).reachableBy(cpg.method.name(methodName).parameter))
-          if (valReachable.size > 0 && indexAccessInAssignment.size > 0 && indexArgument.size > 0 && indexArgumentTainted.size > 0 && identifierOrCall.size > 0 && lastArgumentTainted.size > 0) {
-            idList = idList :+ pp.id
+          breakable {
+            def methodName = pp.location.methodShortName
+            def valReachable = pp.argument(2).reachableBy(cpg.method.name(methodName).parameter.nameNot("this"))
+            def indexAccessInAssignment = pp.astChildren.order(1).isCall.name(Operators.indexAccess)
+            def indexArgument = indexAccessInAssignment.argument(2)
+            def indexArgumentTainted = indexArgument.where(_.reachableBy(cpg.method.name(methodName).parameter.nameNot("this")))
+            def identifierOrCall = indexAccessInAssignment.argument(1)
+            if (identifierOrCall.isIdentifier.nonEmpty) {
+                if (identifierOrCall.evalType("(ANY|.*(O|o)bject|.*\\{.*\\}.*)").size == 0){
+                    break()
+                }
+            }
+            def lastArgumentTainted = identifierOrCall.reachableBy(cpg.call(Operators.indexAccess)).where(_.argument(2).reachableBy(cpg.method.name(methodName).parameter.nameNot("this")))
+            if (valReachable.size > 0 && indexAccessInAssignment.size > 0 && indexArgument.size > 0 && indexArgumentTainted.size > 0 && identifierOrCall.size > 0 && lastArgumentTainted.size > 0) {
+              idList = idList :+ pp.id
+            }
           }
         }
         cpg.call.filter(node => idList.contains(node.id)).l
@@ -56,21 +63,28 @@ object PrototypePollution extends QueryBundle {
         |""".stripMargin,
       score = 8,
       withStrRep({ cpg =>
-        def possiblePollution = cpg.call.where(_.name(Operators.assignment).reachableBy(cpg.method.parameter))
+        def possiblePollution = cpg.call.where(_.name(Operators.assignment).reachableBy(cpg.method.parameter.nameNot("this")))
         var idList : List[Long] = List()
         for (pp <- possiblePollution) {
-          def methodName = pp.location.methodShortName
-          def indexAccessInAssignment = pp.astChildren.order(1).isCall.name(Operators.indexAccess)
-          def indexArgument = indexAccessInAssignment.astChildren.order(2).isIdentifier
-          def indexArgumentTainted = indexArgument.where(_.reachableBy(cpg.method.name(methodName).parameter))
-          def identifierOrCall = if(indexAccessInAssignment.astChildren.order(1).isIdentifier.isEmpty){
-              indexAccessInAssignment.astChildren.order(1).isCall
-          } else {
-              indexAccessInAssignment.astChildren.order(1).isIdentifier
-          }
-          def lastArgumentTainted = identifierOrCall.reachableBy(cpg.call(Operators.indexAccess)).where(_.argument.reachableBy(cpg.method.name(methodName).parameter))
-          if (indexAccessInAssignment.size > 0 && indexArgument.size > 0 && indexArgumentTainted.size > 0 && identifierOrCall.size > 0 && lastArgumentTainted.size > 0) {
-            idList = idList :+ pp.id
+          breakable {
+            def methodName = pp.location.methodShortName
+            def indexAccessInAssignment = pp.astChildren.order(1).isCall.name(Operators.indexAccess)
+            def indexArgument = indexAccessInAssignment.astChildren.order(2).isIdentifier
+            def indexArgumentTainted = indexArgument.where(_.reachableBy(cpg.method.name(methodName).parameter.nameNot("this")))
+            def identifierOrCall = if(indexAccessInAssignment.astChildren.order(1).isIdentifier.isEmpty){
+                indexAccessInAssignment.astChildren.order(1).isCall
+            } else {
+                indexAccessInAssignment.astChildren.order(1).isIdentifier
+            }
+            if (identifierOrCall.isIdentifier.nonEmpty) {
+                  if (identifierOrCall.evalType("(ANY|.*(O|o)bject|.*\\{.*\\}.*)").size == 0){
+                      break()
+                  }
+            }
+            def lastArgumentTainted = identifierOrCall.reachableBy(cpg.call(Operators.indexAccess)).where(_.argument.reachableBy(cpg.method.name(methodName).parameter.nameNot("this")))
+            if (indexAccessInAssignment.size > 0 && indexArgument.size > 0 && indexArgumentTainted.size > 0 && identifierOrCall.size > 0 && lastArgumentTainted.size > 0) {
+              idList = idList :+ pp.id
+            }
           }
         }
         cpg.call.filter(node => idList.contains(node.id)).l
